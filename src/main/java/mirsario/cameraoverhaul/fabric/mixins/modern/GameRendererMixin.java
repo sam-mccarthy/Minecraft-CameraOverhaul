01d88b272a8f2627f8b1c658af265ffae3eacad9
sam-mccarthy <samuelmccarthy03@gmail.com>
2024-06-16 19:59:33 -0400

    Replaced mixin with more portable version

--- src/main/java/mirsario/cameraoverhaul/fabric/mixins/modern/GameRendererMixin.java
+++ src/main/java/mirsario/cameraoverhaul/fabric/mixins/modern/GameRendererMixin.java
@@ -13,23 +13,17 @@ import mirsario.cameraoverhaul.fabric.abstractions.*;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin
 {
-	@Shadow
-	@Final
-	private Camera camera;
+    @Shadow
+    @Final
+    private Camera camera;
 
-	@Inject(method = "renderWorld", at = @At(
-		value = "INVOKE",
-		// Inject before the call to Camera.update()
-		target = "Lnet/minecraft/client/render/Camera;update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V",
-		shift = At.Shift.BEFORE
-	))
-	private void PostCameraUpdate(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci)
-	{
-		Transform cameraTransform = new Transform(camera.getPos(), new Vec3d(camera.getPitch(), camera.getYaw(), 0d));
+    @Inject(method = "bobView", at = @At("TAIL"))
+    private void PostCameraUpdate(MatrixStack matrices, float f, CallbackInfo ci)
+    {
+        Transform cameraTransform = new Transform(camera.getPos(), new Vec3d(camera.getPitch(), camera.getYaw(), 0d));
 
-		cameraTransform = ModifyCameraTransformCallback.EVENT.Invoker().ModifyCameraTransform(camera, cameraTransform);
+        cameraTransform = ModifyCameraTransformCallback.EVENT.Invoker().ModifyCameraTransform(camera, cameraTransform);
 
-		//matrix.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion((float)cameraTransform.eulerRot.z));
-		MathAbstractions.RotateMatrixByAxis(matrix, 0f, 0f, 1f, (float)cameraTransform.eulerRot.z);
-	}
+        MathAbstractions.RotateMatrixByAxis(matrices, 0f, 0f, 1f, (float)cameraTransform.eulerRot.z);
+    }
 }
