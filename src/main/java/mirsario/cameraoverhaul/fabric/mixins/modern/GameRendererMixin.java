7b50211eeb1feff57dfe9bf51728ac6cdbe1dfd5
Mirsario <me@mirsar.io>
2024-06-17 17:03:33 +0300

    Tabs.

--- src/main/java/mirsario/cameraoverhaul/fabric/mixins/modern/GameRendererMixin.java
+++ src/main/java/mirsario/cameraoverhaul/fabric/mixins/modern/GameRendererMixin.java
@@ -13,17 +13,17 @@ import mirsario.cameraoverhaul.fabric.abstractions.*;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin
 {
-    @Shadow
-    @Final
-    private Camera camera;
+	@Shadow
+	@Final
+	private Camera camera;
 
-    @Inject(method = "bobView", at = @At("TAIL"))
-    private void PostCameraUpdate(MatrixStack matrices, float f, CallbackInfo ci)
-    {
+	@Inject(method = "bobView", at = @At("TAIL"))
+	private void PostCameraUpdate(MatrixStack matrices, float f, CallbackInfo ci)
+	{
         Transform cameraTransform = new Transform(camera.getPos(), new Vec3d(camera.getPitch(), camera.getYaw(), 0d));
 
         cameraTransform = ModifyCameraTransformCallback.EVENT.Invoker().ModifyCameraTransform(camera, cameraTransform);
 
         MathAbstractions.RotateMatrixByAxis(matrices, 0f, 0f, 1f, (float)cameraTransform.eulerRot.z);
-    }
+	}
 }
